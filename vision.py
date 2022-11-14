6c4209838560397a5fbea08b104532c378c11528
Nitchanan-K <teelajen111@gmail.com>
2022-11-14 17:34:35 +0700

    second 14 nov 2022

--- vision.py
+++ vision.py
@@ -2,13 +2,25 @@ import cv2
 import numpy as np
 import os
 
-def findClickPositions(base_img_path, obj_img, threshold = 0.969, debug_mode = None):
-    
-    poring_front = cv2.imread('poring_front.png')
-    
-    poring_front = poring_front.shape[1]
-    poring_front = poring_front.shape[0]
 
-    result = cv2.matchTemplate(base_img, poring_front,cv2.TM_CCORR_NORMED)
 
-    pass
+
+def findClickPositions(base_img_path, obj_img_path, threshold = 0.96, debug_mode = None):
+    
+    base_img = cv2.imread(base_img_path)
+    poring_front_img = cv2.imread(obj_img_path)
+
+    poring_w = poring_front_img.shape[1]
+    poring_h = poring_front_img.shape[0]
+
+    result = cv2.matchTemplate(base_img, poring_front_img,cv2.TM_CCORR_NORMED)
+
+    # Get the all the positions from the match result that exceed our threshold
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+    print(locations)
+
+    rectangles = []
+
+
+findClickPositions('base_img.jpg','poring_front.png')
